c667b661675c6d2c1f6e3e588be8e167c63037dd
nawashiro <hooochiba@gmail.com>
2025-08-06 16:52:11 +0900

    fix: 評価ボタンのラベルを「賛成」と「反対」から「いいね」と「よくないね」に変更

--- src/components/discussion/EvaluationComponent.tsx
+++ src/components/discussion/EvaluationComponent.tsx
@@ -160,7 +160,7 @@ export function EvaluationComponent({
                     d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"
                   />
                 </svg>
-                賛成
+                <span className="hidden md:inline">いいね</span>
               </>
             )}
           </button>
@@ -188,7 +188,7 @@ export function EvaluationComponent({
                     d="M10 14H5.236a2 2 0 01-1.789-2.894l3.5-7A2 2 0 018.736 3h4.018c.163 0 .326.02.485.06L17 4m-7 10v2a2 2 0 002 2h.095c.5 0 .905-.405.905-.905 0-.714.211-1.412.608-2.006L17 13V4m-7 10h2m5-10h2a2 2 0 012 2v6a2 2 0 01-2 2h-2.5"
                   />
                 </svg>
-                反対
+                <span className="hidden md:inline">よくないね</span>
               </>
             )}
           </button>
